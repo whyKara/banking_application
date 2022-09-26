b05a885375b2f45aca515ad4a9a6ea30f09d9e65
whyKara <karapurkaryash@gmail.com>
2022-09-26 11:11:24 +0530

    account card linked pages

--- bank/urls.py
+++ bank/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name='Bank Home'),
-    path('account', views.account, name='Bank Account'),
+    path('', views.home, name='Bank-Home'),
+    path('account', views.account, name='Account-Details'),
+    path('card', views.card, name='Card-Details'),
 ]
--- bank/views.py
+++ bank/views.py
@@ -33,12 +33,32 @@ accs = [
 ]
 
 
-def home(request):
+cards = [
+    {
+        'card_type': 'Debit Card',
+        'card_no': '218361983382173712',
+    },
+    {
+        'card_type': 'Credit Card',
+        'card_no': '218361983382173712',
+    },
+]
+
+
+def account(request):
     context = {
         'accs': accs
     }
     return render(request, 'bank/u_account.html', context)
 
 
-def account(request):
-    return HttpResponse('<h1>account</h1>')
+def card(request):
+    context = {
+        'cards': cards
+    }
+    return render(request, 'bank/u_cards.html', context)
+
+
+def home(request):
+    # return render(request, 'bank/base.html')
+    return HttpResponse('<h1>sjyfsuyf</h1>')
