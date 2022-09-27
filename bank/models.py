6679d9f431c000e3bae0e3af4949f82182284709
Aryan270909 <aryan_shetye@persistent.com>
2022-09-27 17:43:37 +0530

    code

--- bank/models.py
+++ bank/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class create_account (models.Model):
+    u_fname=models.charfield(max_length=100)
+    u_lname=models.charfield(max_length=100)
+    u_Acc_number=models.charfield(max_length=100)
+    u_card_type=
+    u_email=models.EmailField()
+    u_contact=models.IntegerField()
+    u_pan_number=models.IntegerField()
+    u_dob=models.DateField()
+    U_gender=
