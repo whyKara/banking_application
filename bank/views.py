5ee8042457528c6ff8748343f5be152249683596
Aryan270909 <aryan_shetye@persistent.com>
2022-09-28 15:47:35 +0530

    code

--- bank/views.py
+++ bank/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from bank.models import create_account
+from .models import create_account
 from django.db import connections
 # Create your views here.
 
@@ -61,15 +61,21 @@ def card(request):
    
 def create_account(request):
     if request.method =='POST':
-        u_fname = request.POST['fname']
-        u_lname = request.POST['lname']    
-        u_email = request.POST['email']
-        u_lname = request.POST['fname']    
-        u_email = request.POST['fname']
-        u_contact = request.POST['cnumber']
-        u_pan_number = request.POST['pnumber']
-        u_dob = request.POST['dob']
+
     return render(request, 'bank/create_account.html',{})
+
+def sign_up(request):
+    if request.method =='POST':
+        u_fname = request.POST.get['fname']
+        u_lname = request.POST.get['lname']    
+        u_email = request.POST.get['email']
+        u_add1=request.POST.get['address1']
+        u_add2=request.POST.get['address2']
+        u_contact = request.POST.get['cnumber']
+        u_pan_number = request.POST.get['pnumber']
+        u_dob = request.POST.get['dob']
+        u_gender=request.POST.get['gender']
+        return render(request, 'bank/sign_up.html',{})
 
 
 def home(request):
