50623e0966af11ea1c4658ef04dac90c6b9f986e
Aryan270909 <aryan_shetye@persistent.com>
2022-10-03 02:34:30 +0530

    updated forms

--- bank/urls.py
+++ bank/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='Bank-Home'),
     path('account', views.account, name='Account-Details'),
     path('card', views.card, name='Card-Details'),
-    # path('create_account', views.create_account, name='Create-account'),
+    path('create_account', views.create_account, name='Create-account'),
     path('sign_up', views.sign_up, name='sign-up'),
 ]
--- bank/views.py
+++ bank/views.py
@@ -59,22 +59,25 @@ def card(request):
     }
     return render(request, 'bank/u_cards.html', context)
    
-# def create_account(request):
-#     if request.method =='POST':
-
-#     return render(request, 'bank/create_account.html',{})
+def create_account(request):
+     if request.method =='POST':
+        fname = request.POST.get('fname1')
+        lname = request.POST.get('lname1')    
+        pan_number = request.POST.get('pnumber1')
+        if request.POST.get('pnumber')==
+        return render(request, 'bank/create_account.html')
 
 def sign_up(request):
     if request.method =='POST':
-        u_fname = request.POST.get['fname']
-        u_lname = request.POST.get['lname']    
-        u_email = request.POST.get['email']
-        u_add1=request.POST.get['address1']
-        u_add2=request.POST.get['address2']
-        u_contact = request.POST.get['cnumber']
-        u_pan_number = request.POST.get['pnumber']
-        u_dob = request.POST.get['dob']
-        u_gen=request.POST.get['gender']
+        u_fname = request.POST.get('fname')
+        u_lname = request.POST.get('lname')    
+        u_email = request.POST.get('email')
+        u_add1=request.POST.get('address1')
+        u_add2=request.POST.get('address2')
+        u_contact = request.POST.get('cnumber')
+        u_pan_number = request.POST.get('pnumber')
+        u_dob = request.POST.get('dob')
+        u_gen=request.POST.get('gender')
         en=User(u_fname=u_fname,u_lname=u_lname,u_email=u_email,u_add1=u_add1,u_add2=u_add2,u_contact=u_contact,u_pan_number=u_pan_number,u_dob=u_dob,u_gender=u_gen)
         return render(request, 'bank/sign_up.html')
 
